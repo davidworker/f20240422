9bd158453dca46550863eedf679e4d907b276918
kindpingwork <kindpingwork@gmail.com>
2024-06-24 20:16:04 +0800

    數字型態

--- 20240624/assets/js/js_type.js
+++ 20240624/assets/js/js_type.js
@@ -7,3 +7,21 @@ console.log(fullName);
 
 let t1 = "I'm \"David\"";
 console.log(t1); // I'm "David"
+
+
+let n1 = 1;
+let n2 = 1.25;
+let n3 = n1 + n2;
+
+console.log(n1, n2, n3);
+
+let sn = '12';
+let mix = sn + n1;
+console.log(mix);
+
+let sn2 = 'a12';
+let snToNum = parseInt(sn2, 10);
+console.log(snToNum, typeof snToNum)
+
+let snToNumFast = +sn;
+console.log(snToNumFast, typeof snToNumFast)
