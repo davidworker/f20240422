ff27a5b3f0e139c950c19efaffb90e81fb60fed0
kindpingwork <kindpingwork@gmail.com>
2024-07-08 20:02:17 +0800

    change and blur

--- 20240708/assets/js/event.js
+++ 20240708/assets/js/event.js
@@ -12,3 +12,20 @@ cLink.addEventListener(clickType, clickCallback)
 // cBtn.addEventListener(clickType, () => {
 //     console.log('clicked.')
 // })
+
+let chAccount = document.querySelector('#ch-account');
+
+chAccount.addEventListener('change', () => {
+    let value = chAccount.value;
+    console.log(value.length);
+    if (value.length > 8) {
+        alert('超過8個了喔!')
+        value = value.substr(0, 8);
+        chAccount.value = value
+    }
+    console.log(`trigger change value is: ${chAccount.value}`)
+})
+
+chAccount.addEventListener('blur', () => {
+    console.log(`trigger blur value is: ${chAccount.value}`)
+})
