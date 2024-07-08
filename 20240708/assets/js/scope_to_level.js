6103cb343fac34cc2d191fac27f49e73cf564354
kindpingwork <kindpingwork@gmail.com>
2024-07-08 21:23:54 +0800

    三種解法

--- 20240708/assets/js/scope_to_level.js
+++ 20240708/assets/js/scope_to_level.js
@@ -6,9 +6,68 @@ let currentLevel = document.querySelector('#current-level');
 
 result.classList.add('d-none')
 
+const ifSolution = (value) => {
+    let level = '不及格';
+
+    if (value >= 90) {
+        level = '甲';
+    }
+
+    if (value >= 80) {
+        level = '乙';
+    }
+
+    if (value >= 70) {
+        level = '丙';
+    }
+
+    if (value >= 60) {
+        level = '丁';
+    }
+
+    return level;
+}
+
+const switchSolution = (value) => {
+    value = Math.floor(value / 10);
+    let level = '不及格'
+    switch (value) {
+        case 10:
+        case 9:
+            level = '甲';
+            break;
+        case 8:
+            level = '乙';
+            break;
+        case 7:
+            level = '丙';
+            break;
+        case 6:
+            level = '丁'
+            break;
+    }
+    return level;
+}
+
+const mappingSolution = (value) => {
+    value = Math.floor(value / 10);
+    let map = {
+        10: '甲',
+        9: '甲',
+        8: '乙',
+        7: '丙',
+        6: '丁',
+    }
+
+    return map[value] ? map[value] : '不及格'
+}
+
 const calc = () => {
     let value = scope.value;
-    let level = '不及格';
+    // let level = ifSolution(value);
+    // let level = mappingSolution(value);
+    let level = switchSolution(value);
+
 
     // 實作等及判斷
 
