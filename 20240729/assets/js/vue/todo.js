7cc6a6830e4fee0808b8d900be713e34c1009d10
kindpingwork <kindpingwork@gmail.com>
2024-07-29 19:08:40 +0800

    homework - 下載實作

--- 20240729/assets/js/vue/todo.js
+++ 20240729/assets/js/vue/todo.js
@@ -57,7 +57,7 @@ const options = {
         async upload() {
             console.log('upload data...');
 
-            let uid = Storage.getUid();
+            let uid = this.uid;
             let data = Storage.getData(uid);
             Swal.fire({
                 title: "數據上傳中",
@@ -87,6 +87,33 @@ const options = {
                 }
 
             }, 1000)
+        },
+        async download() {
+            console.log('download data...');
+
+            let uid = this.uid;
+
+            let result = await Swal.fire({
+                title: '下載確認?',
+                html: '現有數據將被覆蓋',
+                icon: 'question',
+                showCancelButton: true,
+                cancelButtonText: '等等',
+                confirmButtonText: '確定'
+            })
+
+            if (!result.isConfirmed) {
+                return;
+            }
+
+            let response = await API.read(uid);
+            Storage.setData(uid, response.data);
+            Swal.fire({
+                title: '下載成功',
+                html: '數據已從雲端下載',
+                icon: 'success'
+            })
+            this.load();
         }
     },
     mounted() {
--- 20240729/readme.md
+++ 20240729/readme.md
@@ -19,7 +19,7 @@
 async upload() {
     console.log('upload data...');
 
-    let uid = Storage.getUid();
+    let uid = this.uid;
     let data = Storage.getData(uid);
     Swal.fire({
         title: "數據上傳中",
@@ -49,5 +49,44 @@ async upload() {
         }
 
     }, 1000)
+}
+```
+
+### 下載實作
+
+```html
+<div class="text-end mb-2">
+    <button class="btn btn-success me-2" @click.prevent="upload">上傳數據</button>
+    <button class="btn btn-danger" @click.prevent="download">下載數據</button>
+</div>
+```
+
+```js
+async download() {
+    console.log('download data...');
+
+    let uid = this.uid;
+
+    let result = await Swal.fire({
+        title: '下載確認?',
+        html: '現有數據將被覆蓋',
+        icon: 'question',
+        showCancelButton: true,
+        cancelButtonText: '等等',
+        confirmButtonText: '確定'
+    })
+
+    if (!result.isConfirmed) {
+        return;
+    }
+
+    let response = await API.read(uid);
+    Storage.setData(uid, response.data);
+    Swal.fire({
+        title: '下載成功',
+        html: '數據已從雲端下載',
+        icon: 'success'
+    })
+    this.load();
 }
 ```
