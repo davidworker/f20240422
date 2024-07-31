10ea430d9e4e56553210513740b2bd6e4dd5a269
kindpingwork <kindpingwork@gmail.com>
2024-07-31 20:22:58 +0800

    實作註冊

--- firebase_app/public/assets/js/member.js
+++ firebase_app/public/assets/js/member.js
@@ -12,13 +12,28 @@ let config = {
     messagingSenderId: "988898477453",
     appId: "1:988898477453:web:d224eedbb30c09eaa0a9c4"
 };
+
 let app = await App.init(config);
 let auth = new Auth(app);
+
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
 
 const options = {
     data() {
         return {
-            mode: 'login'
+            mode: 'login',
+            login: {
+                email: '',
+                pwd: '',
+            },
+            register: {
+                email: '',
+                pwd: '',
+            }
         }
     },
     methods: {
@@ -27,6 +42,48 @@ const options = {
         },
         toRegister() {
             this.mode = 'register'
+        },
+        async doRegister() {
+            if (!this.register.email || !this.register.pwd) {
+                Swal.fire({
+                    title: '註冊錯誤',
+                    html: '未填寫帳號密碼',
+                    icon: 'error'
+                })
+                return
+            }
+
+            if (!validateEmail(this.register.email)) {
+                Swal.fire({
+                    title: '帳號錯誤',
+                    html: '不合法的Email格式',
+                    icon: 'error'
+                })
+                return
+            }
+
+            let response = await auth.register(this.register.email, this.register.pwd);
+            if (response) {
+                await Swal.fire({
+                    title: '註冊成功',
+                    html: '請使用帳號密碼登入',
+                    icon: 'success'
+                })
+                this.doLogout();
+            } else {
+                Swal.fire({
+                    title: '註冊錯誤',
+                    html: '註冊失敗或帳號已存在',
+                    icon: 'error'
+                })
+            }
+        },
+        async doLogin() {
+            console.log('login')
+        },
+        async doLogout() {
+            await auth.signOut();
+            location.reload();
         }
     }
 }
