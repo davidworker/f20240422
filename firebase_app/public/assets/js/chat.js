789192ffabdcfdaee7141d080b7ed1dd0a17b2b1
kindpingwork <kindpingwork@gmail.com>
2024-07-31 21:13:17 +0800

    簡易聊天室

--- firebase_app/public/assets/js/chat.js
+++ firebase_app/public/assets/js/chat.js
@@ -17,8 +17,59 @@ let app = await App.init(config);
 let database = new Database(app);
 let auth = new Auth(app);
 
-auth.onChange((user) => {
-    new Chat(user, database, auth);
-}, () => {
-    location.href = 'member.html';
-});
+const options = {
+    data() {
+        return {
+            user: {},
+            to: '',
+            users: {
+                'kindping@gmail.com': 'OFDQEDtA6yTpYWVfRcYtx82poGz1',
+                'kindpingwork@gmail.com': 'SZErUk4IebP3swxK59Wfg1MjmOR2'
+            },
+            message: '',
+            messages: {}
+        }
+    },
+    methods: {
+        async doLogout() {
+            await auth.signOut();
+            location.href = 'member.html';
+        },
+        async doSend() {
+            if (!this.message) {
+                return;
+            }
+
+            database.insert(`chat/${this.to}`, { from: this.user.email, message: this.message, timer: this.currentTimer() });
+            this.message = '';
+        },
+        currentTimer() {
+            let d = new Date();
+            let hh = d.getHours();
+            let mm = d.getMinutes();
+
+            if (hh < 10) {
+                hh = '0' + hh
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+
+            return `${hh}:${mm}`
+        }
+    },
+    mounted() {
+        auth.onChange((user) => {
+            this.user = user
+            this.to = this.users[this.user.email];
+            database.listen(`chat/${this.user.uid}`, (data) => {
+                this.messages = data;
+            });
+        }, () => {
+            location.href = 'member.html';
+        });
+    }
+}
+
+Vue.createApp(options).mount('#chat-app');
